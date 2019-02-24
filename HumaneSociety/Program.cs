80ba7097f59b65ba26caf99fae17ce7893600cde
unknown <larrymoua24@yahoo.com>
2019-02-24 13:27:01 -0600

    Test Getshots Method works

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -10,15 +10,9 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             HumaneSocietyDataContext db = new HumaneSocietyDataContext();
             Animal cat = db.Animals.Where(a => a.AnimalId == 17).Single();
-            Query.GetShots(cat);
-          
-=======
-
-            PointOfEntry.Run();            
->>>>>>> 517ab9f44f9f240966fe1d8a42e0a87f199b6144
+            Query.GetShots(cat);       
         }
     }
 }
